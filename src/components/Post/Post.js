d55586cdfda038905b65541695c2a24e88596905
Ilya <ikirsanov.devgmail.com>
2020-08-26 23:22:42 +0300

    some work with routes

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 
 import './Post.css';
 
-const post = (props) => (
+const Post = (props) => (
     <article className="Post"
         onClick={props.clicked}>
         <h1>{props.title}</h1>
@@ -12,4 +13,4 @@ const post = (props) => (
     </article>
 );
 
-export default post;
+export default withRouter(Post);
